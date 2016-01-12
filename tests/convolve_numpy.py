6e18e911e02d5d0d549d25554ab384563ff7ca9e
dboyliao <qmalliao@gmail.com>
2016-01-12 16:49:29 +0800

    Add tests for performance.(fft, convolve, fft-convolve, hilbert transform)

---
+++ tests/convolve_numpy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import time
+import sys
+import numpy as np
+
+M = int(sys.argv[1])
+N = int(sys.argv[2])
+repeat = int(sys.argv[3])
+x = np.array([i for i in range(1, M+1)])
+y = np.array([i for i in range(1, N+1)])
+
+def run_convolve():
+    start = time.time()
+    np.convolve(x, y)
+    end = time.time()
+    return end - start
+
+if __name__ == "__main__":
+
+    r = []
+    for _ in range(repeat):
+        r.append(run_convolve())
+
+    print("input size: \nx:{}\ny:{}".format(M, N))
+    print("\033[1;36mbest of {}: {:.6f} secs\033[0m".format(repeat, min(r)))
+    print("\033[1;35mmean time:{}\033[0m".format(np.mean(r)))
+    print("\033[1;35mstd: {}\033[0m".format(np.std(r)))
