ce846af381b08ff933d983e4bbc9f92544009eab
dboyliao <qmalliao@gmail.com>
2016-03-31 23:26:41 +0800

    Add funny comments and LICENSE.

--- NumSwift/Source/FFT.swift
+++ NumSwift/Source/FFT.swift
@@ -1,3 +1,19 @@
+//
+// Dear maintainer:
+//
+// When I wrote this code, only I and God
+// know what it was.
+// Now, only God knows!
+//
+// So if you are done trying to 'optimize'
+// this routine (and failed),
+// please increment the following counter
+// as warning to the next guy:
+//
+// var TotalHoursWastedHere = 0
+//
+// Reference: http://stackoverflow.com/questions/184618/what-is-the-best-comment-in-source-code-you-have-ever-encountered
+
 import Accelerate
 
 /**
